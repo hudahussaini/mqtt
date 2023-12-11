357c4dea9fa6406dcf40bdb6725e641468bf037c
hudahussaini <hudah@umich.edu>
2023-12-10 21:23:30 -0500

    better but no messages coming to mobile

--- smartlock.py
+++ smartlock.py
@@ -3,7 +3,7 @@ from paho.mqtt import client as mqtt
 
 MQTT_TOPIC_LOCK_SUB= "Mobile"
 MQTT_TOPIC_LOCK_PUB= "Smartlock"
-FINAL_PASSWORD = "QWERTY123"
+FINAL_PASSWORD = "b'QWERTY123'"
 TEMP_PASSWORD = "12345678"
 
 CLIENTID = "SmartLock"
@@ -25,7 +25,7 @@ def lock_door(lock):
     If this was real we would call a function to actually lock the door
     Send signal back to mobile saying door is locked
     """
-    print("Yay you were able to lock the door")
+    print("lock")
     lock.publish(MQTT_TOPIC_LOCK_PUB, "Locking Door")
 
 def unlock_door(lock):
@@ -33,22 +33,22 @@ def unlock_door(lock):
     If this was real we would call a function to actually unlock the door
     Send signal back to mobile saying door is unlocked
     """
-    print("Yay you were able to unlock the door")
+    print("unlock")
     lock.publish(MQTT_TOPIC_LOCK_PUB, "*click* *click* the door has been unlocked")
 
 def check_password(lock, password):
     """
     Check password with final password 
     """
+    print("hellofff")
     if FINAL_PASSWORD == password:
         unlock_door(lock)
         lock.loop_forever(10.0)
         #lock door for saftey after 10 secondsß
-        print("hello")
         lock_door(lock)
     else:
         lock.publish(MQTT_TOPIC_LOCK_PUB, "password is wrong")
-        #temp_password()
+        #temp_password(lock)
 
 def temp_password(lock):
     lock.publish(MQTT_TOPIC_LOCK_PUB, f"Temp Password is {TEMP_PASSWORD}")
