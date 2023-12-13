0b592e70f3c2d0f7a73806c2fc00e0c38e982849
hudahussaini <hudah@umich.edu>
2023-12-12 20:25:35 -0500

    with LWT commented

--- smartlock.py
+++ smartlock.py
@@ -19,6 +19,7 @@ def start_smartlock():
     lock = mqtt.Client(CLIENTID)
     lock.connect(BROKER, PORT)
     lock.subscribe(MQTT_TOPIC_LOCK_SUB)
+    # lock.will_set("Break", "Lock is Broken!")
     return lock
 
 def lock_door(lock):
@@ -35,8 +36,8 @@ def unlock_door(lock):
     Send signal back to mobile saying door is unlocked
     """
     print("Mobile Client is Unlocking")
-    if Temp_Activated == True:
-        lock.publish(MQTT_TOPIC_LOCK_PUB, "Temp password needed")
+    # if Temp_Activated == True:
+    #     lock.publish(MQTT_TOPIC_LOCK_PUB, "Temp password needed")
     lock.publish(MQTT_TOPIC_LOCK_PUB, "*click* *click* the door has been unlocked")
 
 def check_password(lock, strmessage):
@@ -52,14 +53,14 @@ def check_password(lock, strmessage):
         lock.loop_forever(10.0)
         #lock door for saftey after 10 secondsß
         lock_door(lock)
-        if strmessage.startswith("Request to activate temp password:"):
-            Activate_Temp(lock, password)
+        # if strmessage.startswith("Request to activate temp password:"):
+        #     Activate_Temp(lock, password)
     else:
         lock.publish(MQTT_TOPIC_LOCK_PUB, "password is wrong")
         #temp_password(lock)
 
-def Activate_Temp():
-     Temp_Activated = True
+# def Activate_Temp():
+#      Temp_Activated = True
     
 def on_message(client, userdata, msg):
     #print(msg.topic+" "+str(msg.payload))
@@ -77,11 +78,18 @@ def on_message(client, userdata, msg):
     else:
         exit()
 
+# def break_lock(lock):
+#     print("Lock broken")
+#     lock.disconnet()
+
+
 def main():
     lock = start_smartlock()
     lock.loop_start()
     lock.on_message = on_message
     time.sleep(40)
+    #LWT
+    # lock.break_lock(lock)
     lock.disconnect()
     lock.loop_stop()
 
